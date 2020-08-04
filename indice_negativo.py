0a0e99e0adee140ad6908dd60075168db8f34fa9
Leandro Luciani Tavares <leandro.ltavares@gmail.com>
2020-08-04 18:11:06 -0300

    Create indice_negativo.py

---
+++ indice_negativo.py
@@ -0,0 +1,9 @@
+lista = ['Aprendendo', 'com', 'os', 'mestres']
+
+# Python é baseado em índice 0
+print(lista[0]) #Saída: Aprendendo
+
+# Python tem suporte a índices negativos
+print(lista[-1]) #Saída: mestres
+print(lista[-4]) #Saída: Aprendendo
+print(lista[-5]) #Erro: IndexError: list index out of range
